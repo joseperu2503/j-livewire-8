fb1f08040de09cbe26788998f9fb8df0d963948a
Jose Perez <joseperu2503@gmail.com>
2022-03-23 14:01:55 -0500

    commit 1

---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.web')
+
+
+@section('content')
+    <div class="text-center">
+        <h1 class="text-3xl text-gray-700 mb-2 uppercase">Ultimos Cursos</h1>
+        <h2 class="text-xl text-gray-6 00 mb-2">Formate online como profesional en tecnologia</h2>
+        <h3 class="text-lg text-gray-6 00 mb-2">70% de los graduados duplica sus ingresos</h3>
+    </div>
+
+    <livewire:course-list>
+@endsection
